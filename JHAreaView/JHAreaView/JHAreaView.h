a927266f98c8a5a5fb1d28a475a395fa61358f57
Jarhom <332139705@qq.com>
2018-02-07 11:02:42 +0800

    使用实例备注

--- JHAreaView/JHAreaView/JHAreaView.h
+++ JHAreaView/JHAreaView/JHAreaView.h
@@ -6,6 +6,16 @@
 //  Copyright © 2018年 厦门市京朋传媒有限公司. All rights reserved.
 //
 
+//__weak __typeof(self) weakSelf = self;
+//[JHAreaView showAreaViewWithTitle:@"收货地址" provinceId:self.province.regionCode cityId:self.city.regionCode areaId:self.area.regionCode level:level selectBlock:^(JHArea *province, JHArea *city, JHArea *area) {
+//    weakSelf.province = province;
+//    weakSelf.city = city;
+//    weakSelf.area = area;
+//    [weakSelf.btn setTitle:[NSString stringWithFormat:@"%@%@%@",province.regionName?:@"",city.regionName?:@"",area.regionName?:@""] forState:UIControlStateNormal];
+//} cancelBlock:^{
+//
+//}];
+
 #import <UIKit/UIKit.h>
 #import "JHArea.h"
 
